ecb16a39bbd252c844055ccaeac0f8cac4752dab
Hammad-official-2520 <106146562+Hammad-official-2520@users.noreply.github.com>
2022-11-12 12:27:03 +0500

    login complete with firebase

---
+++ firebase.js
@@ -0,0 +1,43 @@
+////////////////////////////import//////////////////////////////////
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+ const firebaseConfig = {
+   apiKey: "AIzaSyCUXMTbWKE8jAc7SerKepTq8BNdAGbizwo",
+   authDomain: "attendence-app-d0e83.firebaseapp.com",
+   projectId: "attendence-app-d0e83",
+   storageBucket: "attendence-app-d0e83.appspot.com",
+   messagingSenderId: "288768015543",
+   appId: "1:288768015543:web:a360e1eb0d29a023372132",
+   measurementId: "G-8SSZ1EN4GE"
+ };
+ const app = initializeApp(firebaseConfig);
+ const analytics = getAnalytics(app);
+ const db = getFirestore(app);
+ const auth = getAuth();
+
+
+   ////////////////////////////login//////////////////////////////////
+{
+
+    const loginBtn = document.getElementById("login-btn");
+    loginBtn.addEventListener("click", () => {
+       let name = document.getElementById("email").value;
+       let password = document.getElementById("password").value;
+       signInWithEmailAndPassword(auth, name, password)
+     .then((userCredential) => {
+       console.log(name)
+       console.log(password)
+       const user = userCredential.user;
+       window.location.href = "admin.html"
+     })
+     .catch((error) => {
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       console.log(errorMessage)
+     });
+    })
+
+}
+////////////////////////////login end//////////////////////////////////
