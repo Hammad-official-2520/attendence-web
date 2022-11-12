6b55e6696c08912bb107bc483f75331ed35b59ff
Hammad-official-2520 <106146562+Hammad-official-2520@users.noreply.github.com>
2022-11-12 20:45:16 +0500

    add data to database

--- admin.js
+++ admin.js
@@ -1,4 +1,5 @@
-let sidebar = document.querySelector(".sidebar");
+try {
+  let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
 
@@ -22,3 +23,36 @@ function menuBtnChange(){
     closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
   }
 }
+
+} catch (error) {
+  
+}
+
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+file.addEventListener('change', function(){
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader(); 
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+    }
+});
+
--- firebase.js
+++ firebase.js
@@ -1,8 +1,29 @@
-////////////////////////////import//////////////////////////////////
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
- import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
- import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import {
+  doc,
+  setDoc,
+  getFirestore,
+  getDoc,
+  collection,
+  getDocs,
+  addDoc,
+  orderBy,
+  onSnapshot,
+  
+
+
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
  const firebaseConfig = {
    apiKey: "AIzaSyCUXMTbWKE8jAc7SerKepTq8BNdAGbizwo",
    authDomain: "attendence-app-d0e83.firebaseapp.com",
@@ -15,7 +36,6 @@
  
 
     const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
     const db = getFirestore(app);
     const auth = getAuth();
 
@@ -50,12 +70,201 @@ try{
 ////////////////////////////login end//////////////////////////////////
 
 
+
+
+try {
+  let createclass = document.getElementById("createclass");
+  createclass.addEventListener("click", ()=>{
+   let manahe = document.getElementById("manahe");
+   manahe.style.display = "none"
+   let courseclasses = document.getElementById("courseclasses")
+   courseclasses.style.display = "none"
+   let manageclass = document.getElementById("manageclass")
+   manageclass.style.display = "flex"
+
+  })
+} catch (error) {
+  
+}
 try{
-    let addstudents = document.getElementById("addstudents");
+  let addstudents = document.getElementById("addstudents");
 addstudents.addEventListener("click", ()=>{
+  let manahe = document.getElementById("manahe");
+  manahe.style.display = "block"
+ let manageclass = document.getElementById("manageclass")
+ manageclass.style.display = "none"
+ let courseclasses = document.getElementById("courseclasses")
+ courseclasses.style.display = "none"
  let manageStudent = document.getElementById("manageStudent");
- manageStudent.style.display = "flex"
+ manageStudent.style.display = "block"
+ 
+
 })
 }catch{
-    console.log("error")
+   console.log("error")
 }
+
+
+
+try {
+  let allclass = document.getElementById("allclass")
+  allclass.addEventListener("click", async ()=>{
+    let manahe = document.getElementById("manahe");
+    manahe.style.display = "block"
+    console.log("hellow")
+    let manageclass = document.getElementById("manageclass")
+    manageclass.style.display = "none"
+    let manageStudent = document.getElementById("manageStudent");
+    manageStudent.style.display = "none"
+    let courseclasses = document.getElementById("courseclasses")
+    courseclasses.style.display = "block"
+    const querySnapshot = await getDocs(collection(db,"courses"));
+    querySnapshot.forEach((doc) => {
+        console.log(doc.data())
+        courseclasses.innerHTML = `
+        <div >
+        <h1>Teacher: <span class="doctime">${doc.data().teachername}</span> </h1>
+        <h2>Timing: <span class="doctime">${doc.data().timing}</span> </h2>
+        <h2>Section: <span class="doctime">${doc.data().section}</span> </h2>
+        <h2>CourseName: <span class="doctime"> ${doc.data().coursename}</span></h2>
+        <h2>Batch Number: <span class="doctime">${doc.data().batchno}</span> </h2>
+        </div>
+        `
+    })
+  
+
+  })
+  
+} catch (error) {
+  
+}
+try {
+  let createclass = document.getElementById("createclassess");
+  createclass.addEventListener("click", function(){
+  let coursename = document.getElementById("coursename").value
+  let teachername = document.getElementById("teacher").value
+  let  timing = document.getElementById("classtime").value
+  let section = document.getElementById("section").value
+  let batchno = document.getElementById("batchnumber").value
+  let classedu = document.getElementById("classschedule").value
+  let firDoc = doc(db, "courses", coursename + batchno + timing);
+   setDoc(firDoc, {
+    coursename : coursename,
+    teachername: teachername,
+    timing : timing,
+    section : section,
+    batchno : batchno,
+    classedu : classedu,
+    uid : coursename + batchno + timing
+  });
+
+  })
+} catch (error) {
+  
+}
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click" ,() => {
+  let name1 = document.getElementById("name1");
+  let fathername = document.getElementById("fathername");
+  let select = document.getElementById("select");
+  let rollno = document.getElementById("rollno");
+  let email = document.getElementById("contact");
+  let cnic = document.getElementById("cnic");
+  let myFile = document.getElementById("file");
+  let file = myFile.files[0];
+  console.log(file)
+  let firDoc = doc(db, "students", + Math.random());
+  setDoc(firDoc, {
+    name1 : name1,
+    fathername : fathername,
+   timing : timing,
+   select : select,
+   batchno : batchno,
+   rollno : rollno,
+   cnic : cnic,
+   email : email,
+ });
+
+
+
+
+
+
+
+
+
+
+   
+      // const  uploadFiles =  (file) => {
+      //   return new Promise((resolve, reject) => {
+      //     const storage = getStorage();
+      //     const auth = getAuth();
+      //     let uid = auth.currentUser.uid;
+      //     const storageRef = ref(storage, `users/${uid}.png`);
+      //     const uploadTask = uploadBytesResumable(storageRef, file);
+      //     uploadTask.on(
+      //       "state_changed",
+      //       (snapshot) => {
+      //         const progress =
+      //           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      //         console.log("Upload is " + progress + "% done");
+      //         switch (snapshot.state) {
+      //           case "paused":
+      //             console.log("Upload is paused");
+      //             break;
+      //           case "running":
+      //             console.log("Upload is running");
+      //             break;
+      //         }
+      //       },
+      //       (error) => {
+      //         reject(error);
+      //       },
+      //       () => {
+      //         getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+      //           resolve(downloadURL);
+      //           let url = await downloadURL
+      //           let firDoc = doc(db, "students", Math.random());
+      //           await setDoc(firDoc, {
+      //             name : name1,
+      //             fathername: fathername,
+      //             select : select,
+      //             contact : contact,
+      //             rollno : rollno,
+      //             classedu : classedu,
+      //             cnic : cnic,
+      //             profile: url
+      //           });
+      //         });
+      //       } 
+      //     ); let url =   uploadFiles(file);
+      //   }); 
+      // };
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+  // let firDoc = doc(db, "students",);
+  //  setDoc(firDoc, {
+  //   name : name1,
+  //   fathername: fathername,
+  //   select : select,
+  //   contact : contact,
+  //   rollno : rollno,
+  //   classedu : classedu,
+  //   cnic : cnic,
+
+   
+  // });
+});
